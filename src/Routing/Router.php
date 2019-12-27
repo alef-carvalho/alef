e80f48aeb549dd23d444f87d0457f877226c70d6
Alef Carvalho <alef@alefcarvalho.com.br>
2019-12-27 00:19:30 -0500

    Directories

---
+++ src/Routing/Router.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace Alef\Routing;
+
+
+class Router
+{
+
+    /**
+     * Http methods supported by the router.
+     *
+     * @var array
+     */
+    public static $supported = [
+        'GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'
+    ];
+
+    public function get()
+    {
+
+    }
+
+    public function head()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function put()
+    {
+
+    }
+
+    public function patch()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function options()
+    {
+
+    }
+
+}
